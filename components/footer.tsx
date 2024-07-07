7276ee64940fb3a8ef5b7751b03076446f1673ee
Lidwina Eurora <lidwinaeurora@Lidwinas-MacBook-Air.local>
2024-07-08 03:45:41 +0700

    feat: Add Responsive Navbar & Footer Components to Layout

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,60 @@
 import React from "react";
+import { FaYoutube, FaInstagram, FaTiktok, FaLinkedin, FaLine } from 'react-icons/fa';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-white dark:bg-zinc-900">
+      <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+        <div className="flex flex-col items-center md:flex-row md:justify-between">
+          <div className="mb-6 md:mb-0">
+            <a href="/" className="flex items-center">
+              <img src="#" className="h-8 mr-3" alt="Logo" />
+              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">UI Battlegrounds</span>
+            </a>
+          </div>
+          <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-6 gap-8 sm:gap-6 text-center md:text-left md:justify-center">
+            <div className="md:col-span-2">
+              <h2 className="mb-6 text-sm font-semibold text-zinc-900 uppercase dark:text-white">More</h2>
+              <ul className="text-zinc-500 dark:text-zinc-400 font-medium">
+                <li className="mb-4">
+                  <a href="#" className="hover:underline">uibg</a>
+                </li>
+                <li>
+                  <a href="#" className="hover:underline">uibg</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <hr className="my-6 border-zinc-200 sm:mx-auto dark:border-zinc-700 lg:my-8" />
+        <div className="flex flex-col items-center sm:flex-row sm:justify-between">
+          <span className="text-sm text-zinc-500 text-center sm:text-left dark:text-zinc-400">© 2024 UI Battlegrounds. All Rights Reserved.</span>
+          <div className="flex justify-center mt-4 sm:mt-0 gap-2">
+            <a href="#" className="text-zinc-500 hover:text-zinc-900 dark:hover:text-white mr-5">
+              <FaYoutube className="w-6 h-6" />
+              <span className="sr-only">YouTube Channel</span>
+            </a>
+            <a href="#" className="text-zinc-500 hover:text-zinc-900 dark:hover:text-white mr-5">
+              <FaInstagram className="w-6 h-6" />
+              <span className="sr-only">Instagram Account</span>
+            </a>
+            <a href="#" className="text-zinc-500 hover:text-zinc-900 dark:hover:text-white mr-5">
+              <FaTiktok className="w-6 h-6" />
+              <span className="sr-only">Tiktok Account</span>
+            </a>
+            <a href="#" className="text-zinc-500 hover:text-zinc-900 dark:hover:text-white mr-5">
+              <FaLinkedin className="w-6 h-6" />
+              <span className="sr-only">LinkedIn Page</span>
+            </a>
+            <a href="#" className="text-zinc-500 hover:text-zinc-900 dark:hover:text-white">
+              <FaLine className="w-6 h-6" />
+              <span className="sr-only">Line Official Account</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,58 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import Link from "next/link";
+import { FaBars } from 'react-icons/fa';
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const pathname = usePathname();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className="bg-white border-zinc-200 dark:bg-zinc-900">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+          <img src="#" className="h-8" alt="Logo" />
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">UI Battlegrounds</span>
+        </a>
+        <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+            <Link href="/auth/login" className="text-white bg-zinc-700 hover:bg-zinc-800 focus:ring-4 focus:outline-none focus:ring-zinc-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-zinc-600 dark:hover:bg-zinc-700 dark:focus:ring-zinc-800">
+                    Login
+            </Link>
+          <button 
+            onClick={toggleMenu}
+            type="button" 
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-zinc-500 rounded-lg md:hidden hover:bg-zinc-100 focus:outline-none focus:ring-2 focus:ring-zinc-200 dark:text-zinc-400 dark:hover:bg-zinc-700 dark:focus:ring-zinc-600" 
+            aria-controls="navbar-cta" 
+            aria-expanded={isOpen ? 'true' : 'false'}
+          >
+            <span className="sr-only">Menu Button</span>
+            <FaBars className="w-10 h-10" />
+          </button>
+        </div>
+        <div className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${isOpen ? 'block text-center' : 'hidden'}`} id="navbar-cta">
+          <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-zinc-100 rounded-lg bg-zinc-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-zinc-800 md:dark:bg-zinc-900 dark:border-zinc-700">
+            <li>
+              <Link href="/" className={`block py-2 px-3 md:p-0 text-zinc-900 rounded hover:bg-zinc-100 md:hover:bg-transparent md:hover:text-zinc-700 md:dark:hover:text-zinc-500 dark:text-white dark:hover:bg-zinc-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-zinc-700`} aria-current={pathname === '/' ? 'page' : undefined}>Home</Link>
+            </li>
+            <li>
+              <Link href="/event" className={`block py-2 px-3 md:p-0 text-zinc-900 rounded hover:bg-zinc-100 md:hover:bg-transparent md:hover:text-zinc-700 md:dark:hover:text-zinc-500 dark:text-white dark:hover:bg-zinc-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-zinc-700`} aria-current={pathname === '/event' ? 'page' : undefined}>Our Events</Link>
+            </li>
+            <li>
+              <Link href="/about" className={`block py-2 px-3 md:p-0 text-zinc-900 rounded hover:bg-zinc-100 md:hover:bg-transparent md:hover:text-zinc-700 md:dark:hover:text-zinc-500 dark:text-white dark:hover:bg-zinc-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-zinc-700`} aria-current={pathname === '/about' ? 'page' : undefined}>About</Link>
+            </li>
+            <li>
+              <Link href="/contact" className={`block py-2 px-3 md:p-0 text-zinc-900 rounded hover:bg-zinc-100 md:hover:bg-transparent md:hover:text-zinc-700 md:dark:hover:text-zinc-500 dark:text-white dark:hover:bg-zinc-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-zinc-700`} aria-current={pathname === '/contact' ? 'page' : undefined}>Contact</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
